5ab219cb769493c9d75c69e96fc36d34eeeb566c
Olga <xotaoshi@gmail.com>
2026-02-15 16:40:14 +0200

    Добавьте файлы проекта.

---
+++ HomeworkViews/Models/User.cs
@@ -0,0 +1,26 @@
+﻿/*
+На странице сайта вывести список пользователей (объекты класса «User»). На странице реализовать возможности:
+
+⦁ Поиск по имени
+
+⦁ Поиск по должности
+
+⦁ Сортировка по возрасту
+
+⦁ Сортировка по заработной плате
+
+При желании, можно использовать AJAX для обработки поиска и сортировки без перезагрузки страницы.
+ */
+
+namespace HomeworkViews.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } =  new Guid();
+        public string Name { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
+        public decimal Salary { get; set; }   = 0;  
+
+    }
+}
---
+++ HomeworkViews/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using HomeworkViews.Models;
+
+namespace HomeworkViews.Services
+{
+    public class UserService
+    {
+        private readonly List<User> _users = new List<User>
+            {
+                new User { Name = "Ольга", Position = "Разработчик", Age = 25, Salary = 3000 },
+                new User { Name = "Дмитрий", Position = "Дизайнер", Age = 30, Salary = 2500 },
+                new User { Name = "Алексей", Position = "Менеджер", Age = 22, Salary = 2000 },
+                new User {Name = "Анна", Position = "Разработчик", Age = 28, Salary = 3500 }
+            };
+
+        public IEnumerable<User> GetUsers()
+        {
+            return _users;
+        }
+        public IEnumerable<User> SearchByName(string name)
+        {
+            return _users.Where(u => u.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+        public IEnumerable<User> SearchByPosition(string position)
+        {
+            return _users.Where(u => u.Position.Contains(position, StringComparison.OrdinalIgnoreCase));
+        }
+        public IEnumerable<User> SortByAge(bool ascending = true)
+        {
+            return ascending ? _users.OrderBy(u => u.Age) : _users.OrderByDescending(u => u.Age);
+        }
+        public IEnumerable<User> SortBySalary(bool ascending = true)
+        {
+            return ascending ? _users.OrderBy(u => u.Salary) : _users.OrderByDescending(u => u.Salary);
+        }
+
+        public User? GetUser(Guid id)
+        {
+            return _users.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
